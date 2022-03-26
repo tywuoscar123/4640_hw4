2254e6d07e6985c9f6143f88b0723a4d10eab860
Oscar <tywu.oscar@gmail.com>
2022-03-26 17:25:25 -0400

    completed handling for incorrect guesses

--- game.php
+++ game.php
@@ -1,23 +1,52 @@
 <?php
 require_once("index.php");
-
-$gameControl = new wordGameController("https://www.cs.virginia.edu/~jh2jf/courses/cs4640/spring2022/wordlist.txt");
+if(!isset($_SESSION['game-control-var'])){
+    $gameControl = new wordGameController("https://www.cs.virginia.edu/~jh2jf/courses/cs4640/spring2022/wordlist.txt");
+    $_SESSION['game-control-var'] = $gameControl;
+}
+echo "<br><br><br>" . $_SESSION['game-control-var']->answer . "<br><br><br>";
 ?>
 <html>
 <body>
-    
 Welcome <?= $_SESSION["name"]; ?>
 <br>
-Your email address is: <?= $_SESSION["email"]; ?>
+Your email address is: <?= $_SESSION["email"] ?>
 <br>
-Number of guesses: <?= $_SESSION["numGuess"] ?>
+Number of guesses: <?php 
+    if(isset($_SESSION["guess-hist"])){
+        echo count($_SESSION["guess-hist"]);
+    }
+    else{
+        echo 0;
+    }
+?>
+<br>
+<br>
+Previous guesses:
+<br>
+<?php
+    echo "<br>";
+    if(!empty($_SESSION["guess-hist"])){
+        foreach($_SESSION["guess-hist"] as $guess) {
+            echo $guess;
+            echo "<br>";
+        }
+        echo "<p>There were " . $_SESSION["commonCharCount"] . " characters in the word<p>";
+        echo "<p>" . $_SESSION["sameLocCount"] . " in the correct position." ;
+        echo "<p>Your guess was " . $_SESSION["guessLen"];
+    }else{
+        echo "No guess history availble.";
+    }
+
+?>
 <br>
 <p>
     Please enter a word below to start guessing
 </p>
 <form action="index.php" method="get">
-Guess <input type="text" name="user-guess"><br>
-
+Guess <input type="text" name="user-guess">
+<input type="submit" value="Submit">
+<br>
 
 </body>
 </html> 
--- index.php
+++ index.php
@@ -1,15 +1,83 @@
 <?php
-//Sources used: https://cs4640.cs.virginia.edu, ...
+//Sources used: https://cs4640.cs.virginia.edu, https://www.geeksforgeeks.org/count-common-characters-in-two-strings/
 session_start();
 
+$_SESSION["guessSuccessful"] = false;
+
 class wordGameController{
-    public $wordList;
-    
+    private $wordList;
+    public $answer;
+    public $userGuessNum = 0;
+    public $userGuesses = array();
     //constructor takes url as input for word list
     function __construct($src) {
         $wordList = preg_split('/\s+/',file_get_contents($src), -1, PREG_SPLIT_NO_EMPTY);
-        //$wordList = explode(" ",file_get_contents($src));
-        print_r($wordList);
+        
+        //get random word from wordlist
+        $this->answer = $wordList[rand(0, count($wordList)-1)];
+        return;
+      }
+
+    function checkAns($guess){
+        $ansLen = strlen($this->answer);
+        $guessLen = strlen($guess);
+
+        //if guess is correct, return
+        if(strcasecmp($guess,$this->answer)  === 0){
+            echo "<br><br><br>" . $guess . "<br><br><br>";
+            echo "<br><br><br>" . $this->answer . "<br><br><br>"; 
+            $_SESSION["guessSuccessful"] = true;
+            return;
+        }
+        $guessFreq = array_fill(0, 26, 0);
+        $ansFreq = array_fill(0, 26, 0);
+        $commonCharCount = 0;
+        $sameLocCount = 0;
+        //check if character exists in string, if yes, increment number stored in position of char (a = 0, z = 25), pos of char is index for array
+        for ($i = 0; $i < $guessLen; $i++) {
+                $guessFreq[ord($guess[$i]) - 97]++;
+        }
+        for ($i = 0; $i < $ansLen; $i++) {
+            $ansFreq[ord($this->answer[$i]) - 97]++;
+        }
+        //compare same pairs that exist
+        for($i = 0; $i < 26; $i++){
+            $commonCharCount += min($guessFreq[$i], $ansFreq[$i]);
+        }
+        //check num of same char with same location
+        for($i = 0; $i < $ansLen; $i++){
+            //break loop if reached the end of a word (when i === len of word)
+            if($i >= $ansLen || $i >= $guessLen){
+                break;
+            }
+            if($this->answer[$i] === $guess[$i]){
+                $sameLocCount++;
+            }
+        }
+        //assign all variables to session, display in game.php
+        $_SESSION["commonCharCount"] = $commonCharCount;
+        $_SESSION["sameLocCount"] = $sameLocCount;
+        //check if longer or shorter
+        if($ansLen < $guessLen){
+            $_SESSION["guessLen"] = "Too long";
+        }else if($ansLen > $guessLen){
+            $_SESSION["guessLen"] = "Too short";
+        }else{
+            $_SESSION["guessLen"] = "The same length ";
+        }
+        echo "<br> PRINT VARS<BR>";
+        echo "<br><br>" . $_SESSION["commonCharCount"] . "<br><br>";
+        echo "<br><br>" . $_SESSION["sameLocCount"] . "<br><br>";
+        echo "<br><br>" . $_SESSION["guessLen"] . "<br><br>";
+        return;
+    }
+
+    function submitAns(){
+        $_SESSION["guess-hist"][] = $_GET["user-guess"];
+        $this->checkAns($_GET["user-guess"]);
+        header("Location: /game.php");
+        die();
+        return;
       }
 }
 
@@ -26,6 +94,11 @@ function basic_info(){
 if(isset($_GET['name']))
 {
     basic_info();
+}
+
+if(isset($_GET['user-guess'])){
+    //call checkAns
+    $_SESSION['game-control-var']->submitAns();
 }
 ?>
 <!DOCTYPE html>
